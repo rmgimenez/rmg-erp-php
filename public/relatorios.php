f9f3aa56d1fcd14687fc4a52ad320c081ab754ac
Ricardo Gimenez <rmgimenez@gmail.com>
2026-02-06 22:25:52 -0300

    Adiciona a biblioteca jQuery no arquivo relatorios.php para suporte a funcionalidades de manipulação de DOM e AJAX. A inclusão é feita antes do carregamento do rodapé, garantindo que a biblioteca esteja disponível para scripts subsequentes.

--- public/relatorios.php
+++ public/relatorios.php
@@ -130,6 +130,7 @@ $tipoUsuario = $_SESSION['usuario_tipo'] ?? 'visitante';
         </div>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <?php include __DIR__ . '/includes/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
