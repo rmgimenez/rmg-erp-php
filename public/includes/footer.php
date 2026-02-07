cf3bae06e26bf0c33cc8624bb105b1a873441f00
Ricardo Gimenez <rmgimenez@gmail.com>
2026-02-06 22:45:54 -0300

    Atualiza a mensagem de crédito no rodapé para incluir a referência à Cantina Santanna, melhorando a clareza sobre a finalidade do desenvolvimento.

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-light text-center py-3 mt-auto">
     <div class="container">
-        <p class="mb-0">Desenvolvido por Ricardo Moura Gimenez</p>
+        <p class="mb-0">Desenvolvido por Ricardo Moura Gimenez para Cantina Santanna</p>
     </div>
 </footer>
 
